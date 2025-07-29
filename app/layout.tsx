b3e45b71d3d29897686ac487aa22edbf18685ce9
fsorace0 <43689912+fsorace0@users.noreply.github.com>
2025-07-28 20:35:24 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: 'I love you Laurel',
+  description: 'I love you Laurel',
 };
 
 export default function RootLayout({
